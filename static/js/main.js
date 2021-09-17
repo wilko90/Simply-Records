9ff36b3df9e044e69ac1f98eccd2d39b591281a2
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-09-17 18:39:52 +0000

    feat: add js for carousel, docs: creat js file

---
+++ static/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function() {
+    $("#news-slider").owlCarousel({
+        items:6,
+        itemsDesktop:[1199,2],
+        itemsDesktopSmall:[980,2],
+        itemsMobile:[700,1],
+        pagination:false,
+        navigation:true,
+        navigationText:["",""],
+        autoPlay:true
+    });
+});
