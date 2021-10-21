df3c9cbc1352664abc04e0980f801fcb2b32c760
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-21 10:29:54 +0000

    feat: add context prcessor

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 
@@ -21,5 +21,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
