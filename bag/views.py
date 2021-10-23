8fd93b6de9d2dd26ef1985fea7011c81696b82e7
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-23 10:03:36 +0000

    docs: finish product format logic

--- bag/views.py
+++ bag/views.py
@@ -13,12 +13,24 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    size = None
+    if 'product_size' in request.POST:
+        format = request.POST['product_format']
     bag = request.session.get('bag', {})
-
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    
+    if size:
+        if item_id in list(bag.keys()):
+            if size in bag[item_id]['items_by_format'].keys():
+                bag[item_id]['items_by_format'][size] += quantity
+            else:
+                bag[item_id]['items_by_format'][size] = quantity
+        else:
+            bag[item_id] = {'items_by_format': {size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
