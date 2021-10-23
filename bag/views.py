8db74037e0567e2f5935ae03552c8de02961198b
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-23 12:35:47 +0000

    feat/docs: added adjust bag and remove baf views, added calc subtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -13,19 +13,19 @@ def add_to_bag(request, item_id):
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
-    size = None
-    if 'product_size' in request.POST:
+    format = None
+    if 'product_format' in request.POST:
         format = request.POST['product_format']
     bag = request.session.get('bag', {})
     
-    if size:
+    if format:
         if item_id in list(bag.keys()):
-            if size in bag[item_id]['items_by_format'].keys():
-                bag[item_id]['items_by_format'][size] += quantity
+            if format in bag[item_id]['items_by_format'].keys():
+                bag[item_id]['items_by_format'][format] += quantity
             else:
-                bag[item_id]['items_by_format'][size] = quantity
+                bag[item_id]['items_by_format'][format] = quantity
         else:
-            bag[item_id] = {'items_by_format': {size: quantity}}
+            bag[item_id] = {'items_by_format': {format: quantity}}
     else:
         if item_id in list(bag.keys()):
             bag[item_id] += quantity
@@ -34,3 +34,51 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+def adjust_bag(request, item_id):
+    """Adjust the quantity of the specified product to the specified amount"""
+
+    quantity = int(request.POST.get('quantity'))
+    format = None
+    if 'product_format' in request.POST:
+        format = request.POST['product_format']
+    bag = request.session.get('bag', {})
+
+    if format:
+        if quantity > 0:
+            bag[item_id]['items_by_format'][format] = quantity
+        else:
+            del bag[item_id]['items_by_format'][format]
+            if not bag[item_id]['items_by_format']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+
+def remove_from_bag(request, item_id):
+    """Remove the item from the shopping bag"""
+
+    try:
+        format = None
+        if 'product_format' in request.POST:
+            format = request.POST['product_format']
+        bag = request.session.get('bag', {})
+
+        if format:
+            del bag[item_id]['items_by_format'][format]
+            if not bag[item_id]['items_by_format']:
+                bag.pop(item_id)
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
