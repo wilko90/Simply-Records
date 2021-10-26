a97c2a8b2218ebf4e8d57f8376c5a7559988ccfc
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-26 21:37:59 +0000

    docs: add function to render pre order catergory to home page

--- home/views.py
+++ home/views.py
@@ -1,9 +1,20 @@
 from django.shortcuts import render
+from products.models import Product
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    try:
+        products = Product.objects.all()
+        home_pre_orders = Product.objects.filter(category__name='pre_orders_funk_soul_jazz')
+    except ObjectDoesNotExist:
+        products = None
 
-    return render(request, 'home/index.html')
 
+    context = {
+        'products': products,
+        'home_pre_orders': home_pre_orders,
+    }
+    return render(request, 'home/index.html', context)
