3075b898f32e3e032cfef3f2aef4b6a03d7c01e1
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-26 10:19:36 +0000

    remove secret key and and set debug

--- simply_records/settings.py
+++ simply_records/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-*7lc!*^ahn40s*5ldqv4_e+_+^u@ji=7*^my1doljk6+l$+w@q'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['simply-records.herokuapp.com', 'localhost']
 
