a69015b523f80dd766e57a1f19ed095a17759e04
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-26 12:05:17 +0000

    docs: sending real emails

--- simply_records/settings.py
+++ simply_records/settings.py
@@ -22,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY', '')
+SECRET_KEY = '%ef%-t4rfb747@#bj+y^vri3et6-aaa=iq3$$#6s6_!@w#9yq_'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = True
 
-ALLOWED_HOSTS = ['simply-records.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = []
 
 
 # Application definition
