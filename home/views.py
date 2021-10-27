77c37943349ce9c57a7299e8263b338a217ef76a
wilko90 <mark_wilkinson2010@hotmail.co.uk>
2021-10-27 13:46:34 +0000

    add equipment and merchnadise category

--- home/views.py
+++ home/views.py
@@ -1,22 +1,26 @@
 from django.shortcuts import render
-from products.models import Product
+from products.models import Product, Category
 from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 def index(request):
     """ A view to return the index page """
+    
     try:
         products = Product.objects.all()
         home_pre_orders = Product.objects.filter(category__name='pre_orders_funk_soul_jazz')
-        merchandise = Product.objects.filter(category__name='slip_mats')
+        equipment = Product.objects.filter(category__name='equipment_products')
+        merchandise = Product.objects.filter(category__name='merchandise_products')
     except ObjectDoesNotExist:
         products = None
+        categories = None
 
 
     context = {
         'products': products,
         'home_pre_orders': home_pre_orders,
+        'equipment': equipment,
         'merchandise': merchandise,
     }
     return render(request, 'home/index.html', context)
